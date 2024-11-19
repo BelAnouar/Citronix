af7aaa76411ea8a47e694b45c4e636a6424e6d48
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2024-11-19 14:27:06 +0100

    CIT-2 config envirennemment

---
+++ src/main/java/org/example/soutnance/SoutnanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.soutnance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutnanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoutnanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/soutnance/SoutnanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.soutnance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoutnanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
